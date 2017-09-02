d577032d5e05cb982f85f5919e6d6e8583ee3879
Henrique Roberto Ricci <30640422+HenriqueRobertoRicci@users.noreply.github.com>
2017-09-02 17:26:47 -0300

    Versão1

---
+++ Locadora_de_Carro/ControllerBicicleta.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import Model.Bicicleta;
+
+public class ControllerBicicleta {
+	public void cadastrar(Bicicleta novaBicicleta) {
+		Bicicleta b = new Bicicleta();
+		b.cadastrarBicicleta(novaBicicleta);
+	}
+
+}
